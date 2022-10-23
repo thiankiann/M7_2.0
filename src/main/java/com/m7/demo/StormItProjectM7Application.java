8859aa17dc09f7a3b7e1d2600146a11e5650f09e
@thiankiann <thiankiann@gmail.com>
2022-10-23 13:26:43 +0100

    Initial Commit

---
+++ src/main/java/com/m7/demo/StormItProjectM7Application.java
@@ -0,0 +1,13 @@
+package com.m7.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StormItProjectM7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StormItProjectM7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/m7/demo/StormItProjectM7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m7.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StormItProjectM7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
